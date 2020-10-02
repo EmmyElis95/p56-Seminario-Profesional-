655c7716ae9e69f714b15eb0c89be0ae60d0b861
Emmy Nuñez <enuneza1@est.ups.edu.ec>
2020-10-02 02:43:06 -0500

    PROYECTO DE EMMY

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv:mongodb+srv://ups:ups2020@cluster0.uhaji.gcp.mongodb.net/utups?retryWrites=true&w=majority'
-    ',
+    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:ups2020@cluster0.uhaji.gcp.mongodb.net/utups?retryWrites=true&w=majority'
+    ,
     port: process.env.PORT || 5000,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/',
